2d60a8bd0b3f518b6eff013f1b7fc4c40c9d793b
KrishTalaviya <93860565+KrishTalaviya@users.noreply.github.com>
2022-07-05 11:29:00 +0530

    Add files via upload

---
+++ insert.sql
@@ -0,0 +1,19 @@
+Create Database Employee_Info_358
+Create table employee(
+EId int,
+EName varchar(100),
+Gender varchar(10),
+JoiningDate Datetime,
+salary Decimal(8,2),
+City varchar(100) 
+)
+Insert into employee values(1,'Nick','Male','13-Jan-01',4000,'London')
+Insert into employee values(2,'Julian','Female','14-Oct-01',3000,'New York')
+Insert into employee values(3,'Roy','Male','16-Jun-01',3500,'London')
+Insert into employee values(4,'Tom','Male',null,4500,'London')
+Insert into employee values(5,'Jerry','Male','13-feb-01',2800,'sydney')
+Insert into employee values(6,'Philip','Male','15-Jan-01',7000,'new york')
+Insert into employee values(7,'Sara','Female','17-aug-01',4800,'Sydney')
+Insert into employee values(8,'Emily','Female','15-Jan-01',5500,'New York')
+Insert into employee values(9,'Michael','Male',null,6500,'London')
+Insert into employee values(10,'John','Male','15-Jan-01',8800,'London')
